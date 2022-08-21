d619d6d9c69f98ecaaae066d81437e11a3dbddb6
Jason <jason.mobley5@gmail.com>
2022-08-20 23:58:42 -0400

    Added checkProgress function; added identifiers to each created entry

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ let submit = document.getElementById('submit')
 
 let index = 0
 let pauseSubmit = true
+let markedRead
 
 let myLibrary = []
 
@@ -37,6 +38,7 @@ submit.addEventListener('click', function(e) {
     let author = bookAuthor.value;
     let pages = pagesRead.value;
     let total = totalPages.value;
+    let id = index
     if (readStatus.checked) {
         progress = true;
     } else {
@@ -53,33 +55,36 @@ submit.addEventListener('click', function(e) {
         }
 
     if (submitReady) {
-        bookInfo = new Book(title, author, pages, total, progress);
+        bookInfo = new Book(title, author, pages, total, progress, id);
         myLibrary.push(bookInfo);
-        console.log(myLibrary);
-        addBookToLibrary();
-        
+        console.log(myLibrary); // Remember to remove
+        createEntry()
+        bookData.style.visibility = 'hidden'
     }
 })
 
 // CONSTRUCTOR
 
-function Book(title, author, pages, total, progress) {
+function Book(title, author, pages, total, progress, id) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.total = total;
     this.progress = progress;
+    this.id = id;
 }
 
 // FUNCTIONS
 
-function addBookToLibrary() {
+function addBookToLibrary() {}
     
+function createEntry() {
+
     let newBook = document.createElement('div');
     let newDiv = document.createElement('h3')
     let newDivTwo = document.createElement('h3')
     let newDivThree = document.createElement('h3')
-    // let newDivFour = document.createElement('h3')
+    let readButton = document.createElement('button')
 
     newBook.classList.add('library-entry');
     
@@ -87,23 +92,60 @@ function addBookToLibrary() {
     newBook.appendChild(newDiv)
     newBook.appendChild(newDivTwo)
     newBook.appendChild(newDivThree)
-    // newBook.appendChild(newDivFour)
+    newBook.appendChild(readButton)
 
     // Assigns classes to new divs
     newDiv.classList.add('new-div-one')
     newDivTwo.classList.add('new-div-two')
     newDivThree.classList.add('new-div-three')
-    // newDivFour.classList.add('new-div-four')
-
+    readButton.classList.add('read-button')
+    readButton.setAttribute("id", 'readButton' + index)
+    
     // Adds text to newly created h3 tags
-    newDiv.textContent = myLibrary[index].title;
-    newDivTwo.textContent = `Illustrated By: ${myLibrary[index].author}`;
-    newDivThree.textContent = `Completed ${myLibrary[index].pages} of ${myLibrary[index].total} pages`;
-    // newDivFour.textContent = myLibrary[index].total;
+     
+     newDiv.textContent = myLibrary[index].title;
+     newDivTwo.textContent = `Illustrated By: ${myLibrary[index].author}`;
+     checkProgress()
+     newDivThree.textContent = `Completed ${pagesRead.value} of ${totalPages.value} pages`;
+     isRead(newBook, readButton)
+     
+     libraryContainer.appendChild(newBook)
+     index ++
+     
+     readButton.addEventListener('click', function() {
+        //  Local event listener for read/unread button
+        newBook.classList.toggle('book-read')
+        readButton.classList.toggle('unread-button')
 
-    libraryContainer.appendChild(newBook)
-    index += 1
+        if (markedRead) {
+            readButton.textContent = "Mark Unread"
+            markedRead = false
+        } else {
+            readButton.textContent = "Mark Read"
+            pagesRead.value = 0
+            markedRead = true
+        }
+     })  
+ }
+
+ function isRead(book, button) {
+    // Checks whether or not book has been completed; changes styling of book entry accordingly
+    if (pagesRead.value == totalPages.value || readStatus.checked) {
+        markedRead = false
+        book.classList.add('book-read')
+        button.classList.add('unread-button')
+        button.textContent = "Mark Unread"
+    } else {
+        button.textContent = "Mark Read"
+        markedRead = true
+    }
 }
 
+function checkProgress() {
+    // Checks readStatus checkbox and changes pagesRead value accordingly
+    if (readStatus.checked) {
+        pagesRead = totalPages
+    }
+}
 
-
+// Find a way to update the value or pagesRead when readStatus is checked
