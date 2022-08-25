27f78f0d853d2ca827bcbc3f2c623697b24df701
Jason <jason.mobley5@gmail.com>
2022-08-24 22:09:04 -0400

    Fixed 'markedRead' issue; deleted unused code

--- script.js
+++ script.js
@@ -1,31 +1,28 @@
 // GLOBAL VARIABLES
-let body = document.querySelector('body')
-let libraryContainer = document.getElementById('libraryContainer')
+let body = document.querySelector('body');
+let libraryContainer = document.getElementById('libraryContainer');
+let addBook = document.getElementById('addBook');
+let bookData = document.getElementById('bookData');
+let closeForm = document.getElementById('closeButton');
+let bookTitle = document.getElementById('bookTitle');
+let bookAuthor = document.getElementById('bookAuthor');
+let pagesRead = document.getElementById('pagesRead');
+let totalPages = document.getElementById('totalPages');
+let readStatus = document.getElementById('readStatus');
+let submit = document.getElementById('submit');
 
-let addBook = document.getElementById('addBook')
-let bookData = document.getElementById('bookData')
-let closeForm = document.getElementById('closeButton')
-let bookTitle = document.getElementById('bookTitle')
-let bookAuthor = document.getElementById('bookAuthor')
-let pagesRead = document.getElementById('pagesRead')
-let totalPages = document.getElementById('totalPages')
-let readStatus = document.getElementById('readStatus')
-let submit = document.getElementById('submit')
+let index = 0;
 
-let index = 0
-let pauseSubmit = true
-let markedRead
-
-let myLibrary = []
+let myLibrary = [];
 
 // EVENT LISTENERS
 
 addBook.addEventListener('click', function() {
-    bookData.style.visibility = 'visible'
+    bookData.style.visibility = 'visible';
 })
 
 closeForm.addEventListener('click', function() {
-    bookData.style.visibility = 'hidden'
+    bookData.style.visibility = 'hidden';
     bookTitle.value = '';
     bookAuthor.value = '';
     pagesRead.value = '';
@@ -33,36 +30,33 @@ closeForm.addEventListener('click', function() {
 })
 
 submit.addEventListener('click', function(e) {
-    let submitReady
+    let submitReady;
     let title = bookTitle.value;
     let author = bookAuthor.value;
     let pages = pagesRead.value;
     let total = totalPages.value;
-    let id = index
-    let progress
+    let id = index;
+    let progress;
     if (readStatus.checked) {
         progress = true;
     } else {
-        progress = false
+        progress = false;
     }
-
-    console.log(progress)
 
     if (title == '' ||
         author == '' ||
         pages == '' ||
         total == '') {
-            submitReady = false
+            submitReady = false;
         } else {
-            submitReady = true
+            submitReady = true;
         }
 
     if (submitReady) {
         bookInfo = new Book(title, author, pages, total, progress, id);
         myLibrary.push(bookInfo);
-        createEntry()
-        console.log(myLibrary)
-        bookData.style.visibility = 'hidden'
+        createEntry();
+        bookData.style.visibility = 'hidden';
     }
 })
 
@@ -78,8 +72,6 @@ function Book(title, author, pages, total, progress, id) {
 }
 
 // FUNCTIONS
-
-function addBookToLibrary() {}
     
 function createEntry() {
 
@@ -89,7 +81,8 @@ function createEntry() {
     let newDivThree = document.createElement('h3');
     let readButton = document.createElement('button');
     let newDelete = document.createElement('button');
-    let idNumber = index
+    let idNumber = index;
+    let markedRead;
     
     newBook.classList.add('library-entry');
     
@@ -106,64 +99,67 @@ function createEntry() {
     newDivThree.classList.add('new-div-three');
     readButton.classList.add('read-button');
     readButton.setAttribute("id", 'readButton' + index);
-    newDelete.classList.add('new-delete')
+    newDelete.classList.add('new-delete');
     
     // Adds text to newly created h3 tags
      newDiv.textContent = myLibrary[index].title;
-     newDivTwo.textContent = `Illustrated By: ${myLibrary[index].author}`;
+     newDivTwo.textContent = `By: ${myLibrary[index].author}`;
      checkProgress();
      newDivThree.textContent = `Completed ${pagesRead.value} of ${totalPages.value} pages`;
-     isRead(newBook, readButton);
-     newDelete.textContent = 'Delete'
+     newDelete.textContent = 'Delete';
+
+     if (pagesRead.value == totalPages.value || readStatus.checked) {
+        markedRead = true;
+        newBook.classList.add('book-read');
+        readButton.classList.add('unread-button');
+        readButton.textContent = "Mark Unread";
+       
+    } else {
+        readButton.textContent = "Mark Read";
+        newBook.classList.remove('book-read');
+        readButton.classList.remove('unread-button');
+        markedRead = false;
+    }
+
      libraryContainer.appendChild(newBook);
      index++
      
      readButton.addEventListener('click', function() {
         // Local event listener for 'mark read/unread' button
-        newBook.classList.toggle('book-read')
-        readButton.classList.toggle('unread-button')
-
-        if (markedRead) {
-            readButton.textContent = "Mark Read"
-            markedRead = false
+        
+        if (markedRead == false) {
+            newBook.classList.add('book-read');
+            readButton.classList.add('unread-button');
+            readButton.textContent = "Mark Unread";
+            markedRead = true;
+    
+            
         } else {
-            readButton.textContent = "Mark Unread"
-            pagesRead.value = 0
-            markedRead = true
+            newBook.classList.remove('book-read');
+            readButton.classList.remove('unread-button');
+            readButton.textContent = "Mark Read";
+            markedRead = false;
         }
+
      })
      
      newDelete.addEventListener('click', function() {
         // Local event listener for 'delete' button
-        newBook.remove()
+        newBook.remove();
         // Removes entry from myLibrary equal to the books idNumber
-        myLibrary = myLibrary.filter(book => book.id != idNumber)
+        myLibrary = myLibrary.filter(book => book.id != idNumber);
         index--
-        console.log(myLibrary)
      })
  }
-
- function isRead(book, button) {
-    // Checks whether or not book has been completed; changes styling of book entry accordingly
-    console.log(pagesRead.value)
-    console.log(totalPages.value)
-    if (pagesRead.value == totalPages.value || readStatus.checked) {
-        markedRead = true
-        book.classList.add('book-read')
-        button.classList.add('unread-button')
-        button.textContent = "Mark Unread"
-    } else {
-        button.textContent = "Mark Read"
-        markedRead = false
-    }
-}
 
 function checkProgress() {
     // Checks readStatus checkbox and changes pagesRead value accordingly
     if (readStatus.checked) {
-        pagesRead.value = totalPages.value
+        pagesRead.value = totalPages.value;
     }
 }
 
 
+// ISSUES TO FIX
 
+// Make mobile-friendly
